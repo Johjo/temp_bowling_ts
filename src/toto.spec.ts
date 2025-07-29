05e245555a2fba9d23b448f19aa5c1c55e99cfbd
Frédéric JAMMES <25798737+fredjammes@users.noreply.github.com>
2025-07-29 14:23:54 +0200

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/toto.spec.ts

--- src/toto.spec.ts
+++ src/toto.spec.ts
@@ -1,6 +1,22 @@
 import { describe, it, expect } from 'vitest';
+
+class Game {
+  roll(number: number) {
+
+  }
+
+  score() {
+    let number = 20;
+    return number;
+  }
+}
+
 describe('coucou', () => {
-  it('should work', () => {
-    expect(true).toBe(false);
+  it('xxx', () => {
+    const game: Game = new Game();
+    for (let i = 0; i < 20; i++)
+      game.roll(1);
+
+    expect(game.score()).toBe(20);
   });
 });
