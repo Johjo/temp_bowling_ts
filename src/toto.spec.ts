e6088cd706e5eb1d6314707d36cafa16da5e7836
Frédéric JAMMES <25798737+fredjammes@users.noreply.github.com>
2025-07-29 15:29:13 +0200

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/toto.spec.ts

--- src/toto.spec.ts
+++ src/toto.spec.ts
@@ -9,13 +9,28 @@ class Game {
 
   score() {
     let score = 0;
-    for (let i = 0; i < this.rolls.length; i++) {
-      if (this.rolls[i] === 10) {
-        score += this.rolls[i + 1] + this.rolls[i + 2];
+    let rollNumber = 0;
+    for (let frameNumber = 0; frameNumber < 10; frameNumber++) {
+      if (this.isStrike(rollNumber)) {
+        score += this.rolls[rollNumber] + (this.rolls[rollNumber + 1] + this.rolls[rollNumber + 2]);
+        rollNumber += 1;
+      } else if (this.isSpare(rollNumber)) {
+        score += this.rolls[rollNumber] + this.rolls[rollNumber + 1] + (this.rolls[rollNumber + 2]);
+        rollNumber += 2;
+      } else {
+        score += this.rolls[rollNumber] + this.rolls[rollNumber + 1];
+        rollNumber += 2;
       }
-      score += this.rolls[i];
     }
     return score;
+  }
+
+  private isSpare(rollNumber: number) {
+    return this.rolls[rollNumber] + this.rolls[rollNumber + 1] === 10;
+  }
+
+  private isStrike(rollNumber: number) {
+    return this.rolls[rollNumber] === 10;
   }
 }
 
@@ -27,23 +42,61 @@ describe('coucou', () => {
 
     expect(game.score()).toBe(20);
   });
-
-  it('', () => {
+  it('xxx', () => {
     const game: Game = new Game();
-    game.roll(1);
-    expect(game.score()).toBe(1);
+    for (let i = 0; i < 20; i++)
+      game.roll(2);
+
+    expect(game.score()).toBe(40);
   });
 
-  it('', () => {
+  it('xxx', () => {
+    const game: Game = new Game();
+    game.roll(5);
+    game.roll(5);
+    for (let i = 0; i < 18; i++)
+      game.roll(1);
+
+    expect(game.score()).toBe(29);
+  });
+
+  it('xxx', () => {
     const game: Game = new Game();
     game.roll(10);
-    game.roll(1);
-    game.roll(1);
-    expect(game.score()).toBe(14);
+    for (let i = 0; i < 18; i++)
+      game.roll(1);
+
+    expect(game.score()).toBe(30);
   });
 
-  it('', () => {
+  it('xxx', () => {
     const game: Game = new Game();
+    game.roll(9);
+    game.roll(1);
+    game.roll(10);
+    game.roll(10);
+    game.roll(10);
+    game.roll(10);
+    game.roll(10);
+    game.roll(10);
+    game.roll(10);
+    game.roll(10);
+    game.roll(10);
+    game.roll(10);
+    game.roll(10);
 
+    expect(game.score()).toBe(290);
   });
+
+  it('xxx', () => {
+    const game: Game = new Game();
+    game.roll(10);
+    game.roll(0);
+    game.roll(1);
+    for (let i = 0; i < 17; i++)
+      game.roll(0);
+
+    expect(game.score()).toBe(12);
+  });
+
 });
